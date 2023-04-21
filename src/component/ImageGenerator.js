7b39b4daaa567ecac1900882038ea858f15456fb
ItsWachira <brianwachira79@gmail.com>
2023-04-21 09:39:49 +0300

    Feat: integrated DALL-E API image generation feature

---
+++ src/component/ImageGenerator.js
@@ -0,0 +1,71 @@
+import '../App.css';
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+
+function ImageGenerator() {
+    const [prompt, setPrompt] = useState("");
+    const [result, setResult] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [placeholder, setPlaceholder] = useState(
+      "Search a lion with Paint Brushes paiting the monalisa paiting..."
+    );
+    const configuration = new Configuration({
+      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    });
+  
+    const openai = new OpenAIApi(configuration);
+  
+    const generateImage = async () => {
+      setPlaceholder(`Search ${prompt}..`);
+      setLoading(true);
+      try {
+        const res = await openai.createImage({
+          prompt: prompt,
+          n: 1,
+          size: "512x512",
+        });
+        setLoading(false);
+        setResult(res.data.data[0].url);
+      } catch (error) {
+        setLoading(false);
+        console.error(`Error generating image: ${error.response.data.error.message}`);
+    }  
+      
+      
+  };
+  
+  return (
+    <div className="container">
+        {loading ? (
+        <>
+          <h3>Generating image... Please Wait...</h3>
+        </>
+      ) : (
+        <>
+          <h2>Generate an Image using Open AI API</h2>
+
+          <textarea
+            className="app-input"
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="100"
+          />
+          <button onClick={generateImage}>Generate an Image</button>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <>
+              
+            </>
+          )}
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ImageGenerator
+
+
